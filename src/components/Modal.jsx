44ad5d2db33a42d65aaa89c5e54dce4c40aa60bb
morshed0099 <mdgolammorshed0099@gmail.com>
2023-05-28 13:03:38 +0600

    modal a complete

---
+++ src/components/Modal.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+
+
+const Modal = ({ data, setPage, pages,page }) => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <table className="table table-striped ">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">id</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        data.map(da => <tr>
+                                            <td>{da.country.name}</td>
+                                            <td>{da.country.id}</td>
+                                        </tr>)
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                        <nav aria-label="...">
+                            <ul class="pagination pagination-lg">
+                                <p>current page {page}</p>
+                                {
+                                    [...Array(pages).keys()].map(number => <button onClick={()=>setPage(number)}
+                                        key={number}
+                                        className={page ===number && 'btn btn-primary btn-sm'}
+                                    >
+
+                                        {number+1}
+                                    </button>)
+                                }
+                            </ul>
+                        </nav>
+                        <div className="modal-footer">
+                            <button type="button" style={{backgroundColor:"#46139f",color:"white"}} className="btn" data-bs-dismiss="modal">Close</button>                          
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Modal;
--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,18 +1,62 @@
-import React from 'react';
+import { Button } from 'bootstrap';
+import React, { useEffect, useState } from 'react';
+import Modal from './Modal';
+
 
 const Problem2 = () => {
+
+    const [page, setPage] = useState(0)
+    const [data, setData] = useState([])
+    const [pages, setPages] = useState()
+    const [modal,setModal]=useState('')
+    const [cournty,setCountry]=useState('United States')
+
+  
+    const url = `https://contact.mediusware.com/api/contacts/?page=${page === 0 ? 1 : page}`
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setData(data.results)             
+                setPages(data.results.length);               
+            })
+    }, [])
+
+    useEffect(() => {
+        fetch(`https://contact.mediusware.com/api/country-contacts/${cournty}/`)
+            .then(res => res.json())
+            .then(data => {
+                setCountry(data.results);
+            })
+    }, [])
+  
+    const handelApiWithCounty = () => { }
 
     return (
 
         <div className="container">
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
+
                 <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+                    <button onClick={()=>setModal('modalA')}  style={{ backgroundColor: "#46139f", color: "white" }} type="button" className="btn" data-bs-toggle="modal" data-bs-target="#exampleModal" >All Contacts</button>
+                    <button onClick={()=>setModal('modalB')} style={{ backgroundColor: "#ff7f50", color: "white" }} type="button" className="btn" data-bs-toggle="modal" data-bs-target="#exampleModal" >US Contacts</button>
                 </div>
-                
+                {
+                    modal ==='modalA' &&
+                    <Modal
+                    data={data}
+                    key={data.id}
+                    setPage={setPage}
+                    pages={pages}
+                    page={page}
+                />
+                }
+                {
+                    modal ==='modalB' &&
+                }
+           
             </div>
         </div>
     );
